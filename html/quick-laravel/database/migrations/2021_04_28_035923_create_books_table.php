968caa8fdc7f1f501468564e34a66554db92c68c
takumi <takumik9823@gmail.com>
2021-04-30 00:29:58 +0900

    8:データベース連携 [8.1]マイグレーション

--- html/quick-laravel/database/migrations/2021_04_28_035923_create_books_table.php
+++ html/quick-laravel/database/migrations/2021_04_28_035923_create_books_table.php
@@ -6,24 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateBooksTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+    // a.マイグレーション実行でbooksテーブルを作成
     public function up()
     {
         Schema::create('books', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('isbn');
+            $table->string('title');
+            $table->integer('price');
+            $table->string('publisher');
+            $table->date('published');
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+    // b.ロールバック時にはbooksテーブルを削除
     public function down()
     {
         Schema::dropIfExists('books');
